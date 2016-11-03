2abbe15dd9b42b626f0b13467de65bc2194e72cf
Nefinity <yatyricky@gmail.com>
2016-11-03 23:46:38 +0800

    新增卡牌属性：版本（类似资料片） excel转json的脚本不再有ABCDE...列号的硬编码

--- design/getJSON.py
+++ design/getJSON.py
@@ -7,6 +7,14 @@ from openpyxl import load_workbook
 wb = load_workbook(filename = 'conflux.xlsx')
 mess = wb['Card']
 
+# Map column headers to A, B, C, D...
+colHeader = {}
+
+colCount = 65
+while (mess[chr(colCount) + "1"].value):
+	colHeader[mess[chr(colCount) + "1"].value] = chr(colCount)
+	colCount += 1
+
 data = []
 
 # print(mess["B" + str(138)].value)
@@ -14,37 +22,39 @@ data = []
 rowCount = 2
 while (mess["B" + str(rowCount)].value):
 	obj = {}
-	obj['id'] = mess["A" + str(rowCount)].value
-	obj['qlvl'] = mess["B" + str(rowCount)].value
-	obj['name'] = mess["C" + str(rowCount)].value
-	obj['color'] = mess["E" + str(rowCount)].value
-	obj['type'] = mess["F" + str(rowCount)].value
-	obj['cost'] = mess["G" + str(rowCount)].value
-	obj['trigger'] = mess["K" + str(rowCount)].value
 
-	if (mess["N" + str(rowCount)].value):
-		obj['flavorText'] = mess["N" + str(rowCount)].value
+	obj['id'] = mess[colHeader['id'] + str(rowCount)].value
+	obj['qlvl'] = mess[colHeader['qlvl'] + str(rowCount)].value
+	obj['name'] = mess[colHeader['name'] + str(rowCount)].value
+	obj['color'] = mess[colHeader['color'] + str(rowCount)].value
+	obj['type'] = mess[colHeader['type'] + str(rowCount)].value
+	obj['cost'] = mess[colHeader['cost'] + str(rowCount)].value
+	obj['trigger'] = mess[colHeader['trigger'] + str(rowCount)].value
+	obj['package'] = mess[colHeader['package'] + str(rowCount)].value
+
+	if (mess[colHeader['flavorText'] + str(rowCount)].value):
+		obj['flavorText'] = mess[colHeader['flavorText'] + str(rowCount)].value
 	else:
 		obj['flavorText'] = ""
 
-	if (mess["I" + str(rowCount)].value):
-		obj['power'] = mess["I" + str(rowCount)].value
+	if (mess[colHeader['power'] + str(rowCount)].value):
+		obj['power'] = mess[colHeader['power'] + str(rowCount)].value
 	else:
 		obj['power'] = 0
 
-	if (mess["J" + str(rowCount)].value):
-		obj['defense'] = mess["J" + str(rowCount)].value
+	if (mess[colHeader['defense'] + str(rowCount)].value):
+		obj['defense'] = mess[colHeader['defense'] + str(rowCount)].value
 	else:
 		obj['defense'] = 0
 
-	if (mess["H" + str(rowCount)].value):
-		obj['subType'] = mess["H" + str(rowCount)].value.split(",")
+	if (mess[colHeader['subType'] + str(rowCount)].value):
+		obj['subType'] = mess[colHeader['subType'] + str(rowCount)].value.split(",")
 	else:
 		obj['subType'] = []
 
 	abilities = []
-	if (mess["L" + str(rowCount)].value):
-		for ability in mess["L" + str(rowCount)].value.split("*"):
+	if (mess[colHeader['description'] + str(rowCount)].value):
+		for ability in mess[colHeader['description'] + str(rowCount)].value.split("*"):
 			token = ability.strip()
 			if (len(token) != 0):
 				abilities.append(token)
