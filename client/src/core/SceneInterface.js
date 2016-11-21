0f52e503ced43dde9dd391b86ea80ab325a95765
sunnycld <443449817@qq.com>
2016-11-21 23:10:32 +0800

    modify project dir

--- client/src/core/SceneInterface.js
+++ client/src/core/SceneInterface.js
@@ -9,22 +9,6 @@
  */
 
 (function(){
-    // var SceneInterface = function(){
-
-    //     var __proto = SceneInterface.prototype;
-
-    //     // 场景进入时由场景管理器调用
-    //     __proto.onLoad = function(){
-
-    //     };
-
-    //     // 场景退出时由场景管理器调用
-    //     __proto.onUnload = function(){
-
-    //     };
-    // };
-
-    // Laya.interface("core.SceneInterface", SceneInterface);
     Laya.interface("core.SceneInterface", null);
 })();
 
